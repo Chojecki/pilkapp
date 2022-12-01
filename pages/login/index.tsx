ca68fff2ebd3a4e53e04136c4d21c243eb04a7e4
Chojecki <mar.chojecki@gmail.com>
2022-12-01 16:26:11 +0100

    tweaks extra

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -38,7 +38,7 @@ export default function Page() {
       <form className="w-full" onSubmit={handleSubmit(onSubmit)}>
         <input
           className="bg-gray-50 my-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          defaultValue="test@o2.pl"
+          defaultValue="admin@pilkapp.pl"
           {...register("email", { required: true })}
         />
 
