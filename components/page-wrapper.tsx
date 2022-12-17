60be4c130f560ffa2c6de34e24b9a897bb14e98c
Chojecki <mar.chojecki@gmail.com>
2022-12-17 18:17:38 +0100

    adding head

--- components/page-wrapper.tsx
+++ components/page-wrapper.tsx
@@ -1,12 +1,20 @@
+import Head from "next/head";
+
 interface PageWrapperProps {
   children: React.ReactNode;
 }
 
 const PageWrapper = ({ children }: PageWrapperProps) => {
   return (
-    <div className="flex p-10 flex-col justify-around items-center  place-items-center bg-gray-100 ">
-      {children}
-    </div>
+    <>
+      <Head>
+        <title>PiłkApp</title>
+        <meta name="description" content="Appka do umawiania się na mecze" />
+      </Head>
+      <div className="flex p-10 flex-col justify-around items-center  place-items-center bg-gray-100 ">
+        {children}
+      </div>
+    </>
   );
 };
 
