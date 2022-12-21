522e60c6992e1ff030ba63a5320c73ca5139c527
Chojecki <mar.chojecki@gmail.com>
2022-12-21 01:04:42 +0100

    tweak

--- components/page-wrapper.tsx
+++ components/page-wrapper.tsx
@@ -11,7 +11,7 @@ const PageWrapper = ({ children }: PageWrapperProps) => {
         <title>PiłkApp</title>
         <meta name="description" content="Appka do umawiania się na mecze" />
       </Head>
-      <div className="flex p-10 flex-col justify-around items-center  place-items-center bg-gray-100 ">
+      <div className="flex p-0 md:p-10 flex-col justify-around items-center  place-items-center bg-gray-100 ">
         {children}
       </div>
     </>
