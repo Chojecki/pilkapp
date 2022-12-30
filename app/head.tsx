b12ec3b2c213598086415c1d17a198e2b5229f64
Chojecki <mar.chojecki@gmail.com>
2022-12-30 16:46:28 +0100

    head

--- app/head.tsx
+++ app/head.tsx
@@ -4,6 +4,15 @@ export default function Head() {
       <title>PiłkApp</title>
       <meta name="description" content="Appka do umawiania się na mecze" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Piłkapp" />
+      <meta
+        property="og:description"
+        content="Appka do umawiania się na mecze"
+      />
+      <meta
+        property="og:image"
+        content="https://pilkapp.vercel.app/_next/image?url=/_next/static/media/logo.8cafd7f8.png&w=2048&q=75"
+      />
     </>
   );
 }
