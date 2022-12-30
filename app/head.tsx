4fb001996ad4c33b67184d340f92e6a9843c917f
Chojecki <mar.chojecki@gmail.com>
2022-12-30 18:38:23 +0100

    next-seo

--- app/head.tsx
+++ app/head.tsx
@@ -1,26 +1,34 @@
+import { NextSeo } from "next-seo";
+
 export default function Head() {
   return (
     <>
-      <title>PiłkApp</title>
-      <meta name="description" content="Appka do umawiania się na mecze" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Piłkapp" />
-      <meta
-        property="og:description"
-        content="Appka do umawiania się na mecze"
+      <NextSeo
+        useAppDir={true}
+        title="PiłkApp"
+        description="Appka do umawiania się na mecze"
+        openGraph={{
+          url: "https://www.pilkapp.pl",
+          title: "PiłkaApp",
+          description: "Appka do umawiania się na mecze",
+          images: [
+            {
+              url: "https://pilkapp.vercel.app/_next/image?url=/_next/static/media/logo.8cafd7f8.png&w=2048&q=75",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+          ],
+          siteName: "PiłkApp",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
       />
-
-      <meta property="og:title" content="Piłkapp" />
-      <meta
-        property="og:description"
-        content="Appka do umawiania się na mecze"
-      />
-      <meta
-        property="og:image"
-        content="https://pilkapp.vercel.app/_next/image?url=/_next/static/media/logo.8cafd7f8.png&w=2048&q=75"
-      />
-      <meta property="og:url" content="https://www.pilkapp.pl" />
-      <meta property="og:type" content="website" />
     </>
   );
 }
