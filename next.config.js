48c8316c481c279390adfb0c75f412f9bc8f15d4
Chojecki <mar.chojecki@gmail.com>
2022-12-31 11:37:42 +0100

    tweaks?

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     appDir: true,
+    runtime: 'experimental-edge',
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
