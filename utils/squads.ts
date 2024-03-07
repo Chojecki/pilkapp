4e779872cc436aa8d9876b926d350b0926917f39
Chojecki <mar.chojecki@gmail.com>
2024-03-07 14:02:33 +0100

    sdf

--- utils/squads.ts
+++ utils/squads.ts
@@ -21,7 +21,6 @@ export const splitPlayers = (players: Player[], game: Game) => {
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
   baseURL: "https://api.together.xyz/v1",
 });
 export const suggestSquadsWithOpenAI = async (
